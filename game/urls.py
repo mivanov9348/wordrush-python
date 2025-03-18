0a2b8debffc2de896a2969d3b53f4748de0fe4a8
mivanov9348 <mivanov93@abv.bg>
2025-03-18 07:39:21 +0200

    add play and high scores

---
+++ game/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('play/', views.play, name='play'),
+    path('check-word/', views.check_word, name='check_word'),
+    path('high-scores/', views.high_scores, name='high_scores'),
+]
